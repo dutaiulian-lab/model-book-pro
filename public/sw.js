4686a425f8c5b887fefd73c98dadf767717592ee
dutaiulian-lab <duta.iulian@gmail.com>
2026-09-10 14:48:34 +0300

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -13,3 +13,4 @@ self.addEventListener('fetch', (e) => {
 // Update cache version 3
 // Update cache version 4 (Perfect Centering)
 // Update cache version 5 (Trimmed Opaque PNG)
+// Update cache version 6 (Scaled up Logo)
